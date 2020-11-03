845a5335fad674a95fca087c43f8cbbec3b0ccaa
Loredana <loredana.cirstea@gmail.com>
2020-11-03 22:43:04 +0200

    Remove non-published code

--- client/src/config.js
+++
@@ -1,1 +0,0 @@
-export const repoLink = 'https://github.com/loredanacirstea/remix-yulp-plugin';
--- client/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  chainWebpack: config => {
-    config.module.rule('eslint').exclude.add(/yulp/);
-  },
-};
